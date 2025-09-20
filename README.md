d01e666584120ec51818974a536e403456e10c8e
Rafael Henrique Rodrigues Da Costa <123227224+RafaelHenriqu@users.noreply.github.com>
2025-09-20 11:44:53 -0300

    Upload

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # More Config
-Uma extensão para o navegador que remove Shorts do YouTube.
+## Youtube com mais liberdade.
+
+
+### Oque é o More Config?
+
+````txt
+Uma extensão para navegador capaz de eliminar certas funcionalidades do YouTube, perfeita para as pessoas que não conseguem ver um short e não ir ver nele.
+````
+
+
+### Objetivo
+````txt
+Eliminar distrações do youtube.
+````
+
+### Funcionalidades
+
+* Remove tudo relacionado aos Shorts no youtube
+* Remove os posts 
---
+++ main.js
@@ -0,0 +1,35 @@
+function Main(){
+  document.querySelectorAll("*").forEach(e=>{
+    if (e.hasAttribute("is-shorts")){ // Remove Shorts da home do youtube mas mantem o botão de shorts ao lado
+      e.remove()
+    }
+
+    if (e.hasAttribute("title")){ // Remove O Botão de Shorts
+      if (e.getAttribute("title") == "Shorts"){
+        e.remove()
+      }
+    }
+    
+    if (e.hasAttribute("is-post")){ // Remove Posts do youtube
+      e.parentElement.parentElement.parentElement.remove()
+    }
+  })
+
+  document.querySelectorAll("ytd-reel-shelf-renderer").forEach(e=>{
+    e.remove()
+  })
+
+ 
+}
+
+// Activeted 
+Main();
+  
+const observer = new MutationObserver(() => {
+  Main();
+});
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true
+});
